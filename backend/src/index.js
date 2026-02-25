1097eacf87e2b46fdcb00bb7013c9d0eed7971cf
KrishGoyal06 <krishgoyal2304@gmail.con>
2026-02-25 23:34:34 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json for project dependencies and scripts.
    - Included Vite logo SVG in public directory.
    - Created App.css for styling the application.
    - Developed App.jsx as the main application component.
    - Added React logo SVG in assets directory.
    - Set up index.css to import Tailwind CSS.
    - Implemented main.jsx to render the App component.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error: ", err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is Running at ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database Connection Failed!! ", err);
+  });
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">
+        Voter Education and Access Platform
+      </h1>
+    </>
+  );
+}
+
+export default App;
